4874bbad7933cd05c744234e69c1e476fc162773
Alex Connell <83471521+AlexMakesSoftware@users.noreply.github.com>
2023-11-17 19:35:32 +0000

    add logging to system.

--- TMS_Pro/production_settings.py
+++ TMS_Pro/production_settings.py
@@ -15,3 +15,31 @@ DATABASES = {
         'NAME': BASE_DIR / 'prod_db.sqlite3',
     }
 }
+
+# Log any errors and warnings to a rolling file in /var/log/django/tms_log.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+        },
+    },
+    'handlers': {
+        'rolling_file': {
+            'level': 'WARNING',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': '/var/log/django/tms.log',
+            'maxBytes': 1024 * 1024 * 5, # i.e. 5 MB
+            'backupCount': '3',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['rolling_file'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    },
+}
